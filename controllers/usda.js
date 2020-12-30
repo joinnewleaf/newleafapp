8739c7ad9c97e47611f4708ebbeb0ab5610ab096
Brad Gershkowitz <bradgershkowitz@MacBook-Pro-6.local>
2020-12-29 19:21:49 -0600

    fix food search

--- controllers/usda.js
+++ controllers/usda.js
@@ -6,190 +6,174 @@
 var request = require("request");
 
 //provide the search request
-function searchFoodDetails(searchId, callback) {
+function searchFood(search, callback) {
+  //set api key
+  var apiKey = process.env.USDA_API_KEY;
 
-    //set api key
-    var apiKey = process.env.USDA_API_KEY
+  //sends url and request
+  //got rid of content type
+  var options = {
+    method: "POST",
+    url: "https://api.nal.usda.gov/fdc/v1/search",
+    qs: { api_key: apiKey },
+    headers: {
+      "Postman-Token": "05620ff6-66a4-4079-9eb9-d0abfa0a8ccb",
+      "cache-control": "no-cache",
+    },
+    body: { generalSearchInput: "'" + search + "'" },
+    json: true,
+  };
 
-    var options = {
-        method: 'GET',
-        url: 'https://api.nal.usda.gov/fdc/v1/' + searchId,
-        qs: { api_key: apiKey },
-        headers:
-        {
-            'Postman-Token': '8e0ee126-2ec6-4d51-a35d-544156783978',
-            'cache-control': 'no-cache'
-        }
-    };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+    var searchCounter = 0;
+    var str1 = "";
+    var str2 = "";
+    var fullObjString = '{"foods": [';
 
-        //parse the response
-        var details = JSON.parse(body);
+    if (body["foods"]) {
+      //going to loop through the body and pull all the foods and ids
+      body["foods"].forEach(function (data) {
+        // get individual description
+        str1 = data["description"].toString();
+        str1 = str1.replace(/\\/g, "\\\\").replace(/\"/g, '\\"'); //to escape double quotes
+        // get individual id
+        str2 = data["fdcId"].toString();
+        str2 = str2.replace(/\\/g, "\\\\").replace(/\"/g, '\\"'); //to escape double quotes
 
-        let counter = 0;
+        //add to object string
+        fullObjString =
+          fullObjString +
+          '{"description": "' +
+          str1 +
+          '","fdcId": "' +
+          str2 +
+          '"}';
 
-        //initialize a string for each detail
-        let str2 = {};
+        //if we finish the loop, return the search results
+        if (searchCounter == body["foods"].length - 1) {
+          fullObjString = fullObjString + "]}";
 
-        //initialize empty json object
-        var fullObjString = {
-            "details": [
-                {
-                    "proteins": 0,
-                    "fats": 0,
-                    "carbs": 0,
-                    "calories": 0,
-                    "sugars": 0,
-                    "sodium": 0
-                }
-            ]
-        }
+          let responseSearch = JSON.parse(fullObjString);
 
-
-        //if the returned value has foodNutrients
-        if (details['foodNutrients']) {
-
-            //some of the energy calories get printed out twice so need to check the first one only
-            let checkEnergy = true;
-
-            //for each foodNutrient, check the value
-            details['foodNutrients'].forEach(function (data) {
-                if (typeof data != undefined) {
-
-                    //grab the nutrient name
-                    let str1 = data["nutrient"]["name"].toString();
-                    str1 = str1.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');//escapes any quotation marks
-
-                    //check if string matches
-                    switch (str1) {
-                        case 'Protein':
-                            str2 = (data["amount"]);
-                            fullObjString['details'][0]['proteins'] = str2;
-                            break;
-                        case 'Total lipid (fat)':
-                            str2 = (data["amount"]);
-                            fullObjString['details'][0]['fats'] = str2;
-
-                            break;
-                        case 'Carbohydrate, by difference':
-                            str2 = (data["amount"]);
-                            fullObjString['details'][0]['carbs'] = str2;
-                            break;
-                        case 'Energy':
-
-                            //only check if we havent yet
-                            if (checkEnergy) {
-                                str2 = (data["amount"]);
-                                fullObjString['details'][0]['calories'] = str2;
-                                checkEnergy = false;
-                            } else {
-                                //no longer need to check
-                                console.log("no longer need to check")
-                            }
-
-                            break;
-                        case 'Sugars, total including NLEA':
-                            str2 = (data["amount"]);
-                            fullObjString['details'][0]['sugars'] = str2;
-                            break;
-                        case 'Sodium, Na':
-                            str2 = (data["amount"]);
-                            fullObjString['details'][0]['sodium'] = str2;
-                            break;
-                    }
-
-                    counter++;
-
-                    //at the end of the string pass the object to the callback function
-                    if (counter == details['foodNutrients'].length) {
-                        callback(fullObjString);
-                    }
-
-                } else {
-                    callback(fullObjString);
-                }
-
-            });
-
-
+          //return callback with the search results
+          callback(responseSearch);
         } else {
-
-            callback(fullObjString);
+          fullObjString = fullObjString + ",";
         }
 
-    });
-
+        searchCounter++;
+      });
+    } else {
+      callback({});
+    }
+  });
 }
 
-
 //provide the search request
-function searchFood(search, callback) {
+function searchFoodDetails(searchId, callback) {
+  //set api key
+  var apiKey = process.env.USDA_API_KEY;
 
-    //set api key
-    var apiKey = process.env.USDA_API_KEY
+  var options = {
+    method: "GET",
+    url: "https://api.nal.usda.gov/fdc/v1/" + searchId,
+    qs: { api_key: apiKey },
+    headers: {
+      "Postman-Token": "8e0ee126-2ec6-4d51-a35d-544156783978",
+      "cache-control": "no-cache",
+    },
+  };
 
-    //sends url and request
-    //got rid of content type
-    var options = {
-        method: 'POST',
-        url: 'https://api.nal.usda.gov/fdc/v1/search',
-        qs: { api_key: apiKey },
-        headers:
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    //parse the response
+    var details = JSON.parse(body);
+
+    let counter = 0;
+
+    //initialize a string for each detail
+    let str2 = {};
+
+    //initialize empty json object
+    var fullObjString = {
+      details: [
         {
-            'Postman-Token': '05620ff6-66a4-4079-9eb9-d0abfa0a8ccb',
-            'cache-control': 'no-cache'
+          proteins: 0,
+          fats: 0,
+          carbs: 0,
+          calories: 0,
+          sugars: 0,
+          sodium: 0,
         },
-        body: { generalSearchInput: "\'" + search + "\'" },
-        json: true
+      ],
     };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+    //if the returned value has foodNutrients
+    if (details["foodNutrients"]) {
+      //some of the energy calories get printed out twice so need to check the first one only
+      let checkEnergy = true;
 
-        var searchCounter = 0;
-        var str1 = "";
-        var str2 = "";
-        var fullObjString = "{\"foods\": [";
+      //for each foodNutrient, check the value
+      details["foodNutrients"].forEach(function (data) {
+        if (typeof data != undefined) {
+          //grab the nutrient name
+          let str1 = data["nutrient"]["name"].toString();
+          str1 = str1.replace(/\\/g, "\\\\").replace(/\"/g, '\\"'); //escapes any quotation marks
 
-        if (body['foods']) {
+          //check if string matches
+          switch (str1) {
+            case "Protein":
+              str2 = data["amount"];
+              fullObjString["details"][0]["proteins"] = str2;
+              break;
+            case "Total lipid (fat)":
+              str2 = data["amount"];
+              fullObjString["details"][0]["fats"] = str2;
 
-            //going to loop through the body and pull all the foods and ids
-            body['foods'].forEach(function (data) {
+              break;
+            case "Carbohydrate, by difference":
+              str2 = data["amount"];
+              fullObjString["details"][0]["carbs"] = str2;
+              break;
+            case "Energy":
+              //only check if we havent yet
+              if (checkEnergy) {
+                str2 = data["amount"];
+                fullObjString["details"][0]["calories"] = str2;
+                checkEnergy = false;
+              } else {
+                //no longer need to check
+                console.log("no longer need to check");
+              }
 
-                // get individual description 
-                str1 = data['description'].toString();
-                str1 = str1.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');//to escape double quotes
-                // get individual id
-                str2 = data['fdcId'].toString();
-                str2 = str2.replace(/\\/g, '\\\\').replace(/\"/g, '\\"');//to escape double quotes
+              break;
+            case "Sugars, total including NLEA":
+              str2 = data["amount"];
+              fullObjString["details"][0]["sugars"] = str2;
+              break;
+            case "Sodium, Na":
+              str2 = data["amount"];
+              fullObjString["details"][0]["sodium"] = str2;
+              break;
+          }
 
-                //add to object string
-                fullObjString = fullObjString + "{\"description\": \"" + str1 + "\",\"fdcId\": \"" + str2 + "\"}";
+          counter++;
 
-                //if we finish the loop, return the search results
-                if (searchCounter == body['foods'].length - 1) {
-                    fullObjString = fullObjString + "]}";
-
-                    let responseSearch = JSON.parse(fullObjString);
-
-                    //return callback with the search results
-                    callback(responseSearch);
-                }
-                else {
-                    fullObjString = fullObjString + ",";
-                }
-
-                searchCounter++;
-            });
-
+          //at the end of the string pass the object to the callback function
+          if (counter == details["foodNutrients"].length) {
+            callback(fullObjString);
+          }
         } else {
-            callback({});
+          callback(fullObjString);
         }
-
-    });
-
+      });
+    } else {
+      callback(fullObjString);
+    }
+  });
 }
 
 //export this to be used in other files
